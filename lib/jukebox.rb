e991170ce9e55e9453f51f9a7c7ca25491d0b8d2
Luis Torres <luistjr@gmail.com>
2020-10-19 14:50:22 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,6 @@ def help
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
-  binding.pry
 end 
 
 def play
