bf9ca69f64f7f75d90f79825b911e1bb2e450a24
Luis Torres <luistjr@gmail.com>
2020-10-19 15:01:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -43,7 +43,23 @@ def exit_jukebox
   puts "Goodbye"
 end 
 
-def run 
-  puts "Please enter a command"
-end 
+def run(songs)
+  while true do
+    puts "Please enter a command:"
+    response = gets.chomp
+    case response
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
+    end
+  end
+end
 
